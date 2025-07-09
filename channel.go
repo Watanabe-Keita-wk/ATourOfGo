72ec012803655153a4e63c666bad8aa3838b9d7f
watanabekeit <watanabekeit@istyle.co.jp>
2025-07-10 08:56:49 +0900

    buffer, close

--- channel.go
+++ channel.go
@@ -10,6 +10,24 @@ func sum(s []int, c chan int) {
 	c <- sum
 }
 
+func buffer() {
+	ch := make(chan int, 2)
+	ch <- 1
+	ch <- 2
+	// ch <- 3 //ここで詰まる
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+}
+
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
+	}
+	close(c)
+}
+
 func main() {
 	s := []int{7, 2, 8, -9, 4, 0}
 
@@ -19,4 +37,12 @@ func main() {
 	x, y := <-c, <-c
 
 	fmt.Println(x, y, x+y)
+
+	buffer()
+
+	c2 := make(chan int, 10)
+	go fibonacci(cap(c2), c2)
+	for i := range c2 {
+		fmt.Println(i)
+	}
 }
